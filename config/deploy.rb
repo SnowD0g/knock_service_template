950a59a168140c3825f6555ba06a8007c2c172a8
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-19 22:44:21 +0100

    Create deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,14 @@
+set :application, "%application_name%"
+set :repo_url, "%remote_repo%"
+
+set :rvm_ruby_version, "%ruby_version%@%application_name%"
+
+set :passenger_rvm_ruby_version, fetch(:rvm_ruby_version)
+set :passenger_restart_command, 'touch tmp/restart.txt'
+set :passenger_restart_options, ''
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+#append :linked_files, 'config/master.key'
+#append :linked_files, 'config/.env'
+
+set :keep_releases, 3
