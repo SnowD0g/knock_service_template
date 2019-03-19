05582bb57bad2cc0a3fe0a442fbc859b07720532
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-20 00:07:46 +0100

    Create production.rb

---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+server 'ns3051471.ovh.net', user: 'web', roles: %w{app db web}
+set :stage, :production
+set :branch, :master
+set :deploy_to, "/home/web/www/#{fetch(:application)}"
+
+set :ssh_options, {
+    keys: %w(/home/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(password)
+}
