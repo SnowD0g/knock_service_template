5034153007b37b9ea3a5465e26f5f09200176279
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-19 17:48:58 +0100

    Update sidekiq.rb

--- shared/sidekiq.rb
+++ shared/sidekiq.rb
@@ -1,6 +1,6 @@
 def sidekiq
   copy_file 'config/sidekiq.yml'
-  gsub_file('config/sidekiq.yml', /%app_name%/, appname)
+  gsub_file('config/sidekiq.yml', /%app_name%/, application_name)
 end
 
 sidekiq
