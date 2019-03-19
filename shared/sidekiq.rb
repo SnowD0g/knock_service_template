be482cd63010ba180fc1745585b3738aaca21187
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-19 18:19:45 +0100

    Update sidekiq.rb

--- shared/sidekiq.rb
+++ shared/sidekiq.rb
@@ -1,5 +1,6 @@
 def sidekiq
   copy_file 'config/sidekiq.yml'
+  directory "tmp/pids", force: true
   run "touch tmp/pids/sidekiq.pid"
   gsub_file('config/sidekiq.yml', /%application_name%/, application_name)
 end
