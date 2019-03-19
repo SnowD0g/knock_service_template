e1eda4dd7ef335d47df95e5532bb13bc707ad18b
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-19 15:59:40 +0100

    Update database.rb

--- shared/database.rb
+++ shared/database.rb
@@ -28,6 +28,15 @@ end
 
 def configure_mysql
   copy_file 'config/mysql/database.yml', 'config/database.yml'
+  db_username =  ask("\n[Database Config][1/4] Nome Utente ? (mysql)")
+  db_username = 'postgres' unless db_username.present?
+  db_name = ask("\n[Database Config][2/4] Nome database ? (#{application_name})")
+  db_name = application_name unless db_name.present?
+  db_port = ask("\n[Database Config][3/4] Porta del servizio ? (32784)")
+  db_port = '32770' unless db_port.present?
+  gsub_file('config/database.yml', /%username%/, db_username)
+  gsub_file('config/database.yml', /%port%/, db_port)
+  gsub_file('config/database.yml', /%application_name%/, db_name)
 end
 
 def enable_pg_uuid_extension
