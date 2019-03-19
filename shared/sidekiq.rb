83cfe79e64b860feb08272295f2171d06d157f72
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-19 23:57:43 +0100

    Update sidekiq.rb

--- shared/sidekiq.rb
+++ shared/sidekiq.rb
@@ -3,8 +3,7 @@ def sidekiq
   run "mkdir -p tmp/pids"
   run "touch tmp/pids/sidekiq.pid"
   gsub_file('config/sidekiq.yml', /%application_name%/, application_name)
-  initializer 'sidekiq.rb', ''
-  Sidekiq.configure_server { |config| config.redis = { url: 'redis://localhost:32769' } }
+  initializer 'sidekiq.rb', "Sidekiq.configure_server { |config| config.redis = { url: 'redis://localhost:32769' } }"
 end
 
 sidekiq
