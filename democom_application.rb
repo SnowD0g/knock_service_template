a8bf99b99b0b540e4a2276b039a7fff83d5e5f30
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-19 23:45:50 +0100

    Update democom_application.rb

--- democom_application.rb
+++ democom_application.rb
@@ -1,10 +1,13 @@
-require 'rails/template_runner'
-
 class DemocomApplication
   attr_reader :application_name, :ruby_version
 
   def initialize
     @application_name = ask('Nome Applicazione:').underscore
     @ruby_version = ask('Versione ruby:').underscore
+  end
+  
+  def ask(string)
+    log '', string
+    STDIN.gets.strip
   end
 end
