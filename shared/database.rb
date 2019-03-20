b58c28a4a35b013da1fff1150116c6cb4137cef5
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-20 01:56:00 +0100

    Update database.rb

--- shared/database.rb
+++ shared/database.rb
@@ -14,12 +14,9 @@ end
 
 def configure_postgresql
   copy_file 'config/postgresql/database.yml', 'config/database.yml'
-  db_username =  ask("\n[Database Config][1/4] Nome Utente ? (postgres)")
-  db_username = 'postgres' unless db_username.present?
-  db_name = ask("\n[Database Config][2/4] Nome database ? (#{application_name})")
-  db_name = application_name unless db_name.present?
-  db_port = ask("\n[Database Config][3/4] Porta del servizio ? (32770)")
-  db_port = '32770' unless db_port.present?
+  db_username =  ask_with_default("\n[Database Config][1/4] Nome Utente", 'postgres')
+  db_name = ask_with_default("\n[Database Config][2/4] Nome database", application_name)
+  db_port = ask_with_default("\n[Database Config][3/4] Porta del servizio", '32770')
   gsub_file('config/database.yml', /%username%/, db_username)
   gsub_file('config/database.yml', /%port%/, db_port)
   gsub_file('config/database.yml', /%application_name%/, db_name)
