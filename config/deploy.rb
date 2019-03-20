dbb85c0a4dbf3ccca6b40fc9b8c3836cf3f0e60f
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-20 17:23:23 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,9 @@ require "rvm/capistrano"
 set :application, "%application_name%"
 set :repo_url, "%repo_path%"
 
-set :rvm_ruby_string, "%ruby_version%@%application_name%"
+set :rvm_ruby_version, "%ruby_version%@%application_name%"
 
-set :passenger_rvm_ruby_version, fetch(:rvm_ruby_string)
+set :passenger_rvm_ruby_version, fetch(:rvm_ruby_version)
 set :passenger_restart_command, 'touch tmp/restart.txt'
 set :passenger_restart_options, ''
 
@@ -14,3 +14,9 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 #append :linked_files, 'config/.env'
 
 set :keep_releases, 3
+
+task :create_gemset do
+  on roles(:app) do
+    execute :sudo, :rvm, "use #{fetch(:rvm_ruby_version)} --create"
+  end
+end
