8cc7b9fa44a4ea550c34b1b556c726e2b2df5fc7
Marco Iannantuono <marco.iannantuono@gmail.com>
2019-03-20 10:49:34 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,4 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 #append :linked_files, 'config/master.key'
 #append :linked_files, 'config/.env'
 
-before 'deploy', 'rvm:create_gemset'
-
 set :keep_releases, 3
